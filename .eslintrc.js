dcdf2a7e53ec75716a4ffdc7045d20b79fb9c36a
Jackie098 <carlosmiranda19122@gmail.com>
2020-06-14 10:48:39 -0300

    configured eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true
+    },
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly"
+    },
+    extends: [
+      "airbnb",
+      'eslint:recommended',
+      "prettier/@typescript-eslint",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 11,
+      sourceType: "module",
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.json'],
+      ecmaFeatures: {
+        jsx: true,
+      }
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint",
+        "prettier",
+    ],
+    rules: {
+      // "import/extensions": [
+      //   "error",
+      //   "ignorePackages",
+      //   {
+      //     "js": "never",
+      //     "jsx": "never",
+      //     "ts": "never",
+      //     "tsx": "never",
+      //   }
+      // ],
+      "prettier/prettier": "error",
+      "no-console": "warn",
+      "no-eval": "error",
+      "import/first": "error",
+      "react/jsx-filename-extension": [
+        1,
+        { "extensions": [".js", ".jsx", ".ts", ".tsx"] }
+      ],
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        "node": {
+          "extensions": [".js", ".jsx", ".ts", ".tsx"]
+        }
+      }
+    }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
+
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
