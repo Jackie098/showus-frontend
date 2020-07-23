002aef1308428a5bea0fcaa401d65efb03d257c3
Carlos A <carlosmiranda19122@gmail.com>
2020-07-23 15:26:58 -0300

    added the Ads component

---
+++ src/components/Ads/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// interface Props {
+//   message?: string;
+// }
+
+import { Container } from './styles';
+
+const Ads: React.FC = () => {
+  return (
+    <Container>
+      <h4>Simples e rápido, você só precisa escolher o que quer comer!!!</h4>
+    </Container>
+  );
+};
+
+export default Ads;
---
+++ src/components/Ads/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100px;
+  border-bottom: 1px solid #c5c5c5;
+
+  font-size: 30px;
+  font-weight: bold;
+
+  color: var(--text-color);
+  background: var(--container-color);
+
+  h4 {
+    max-width: 500px;
+    text-align: center;
+  }
+`;
